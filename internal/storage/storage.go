c5f8a11a4a0ce70da0022d144488ee6424616a94
gempellm <lev_gempel@inbox.ru>
2022-09-12 15:01:21 +0300

    Add upload method

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,7 +1,20 @@
 package storage
 
+import "github.com/gempellm/storage/internal/file"
+
 type Storage struct{}
 
 func NewStorage() *Storage {
 	return &Storage{}
+}
+
+func (s *Storage) Upload(filename string, blob []byte) (*file.File, error) {
+	return file.NewFile(filename, blob)
+
+	// newFile, err := file.NewFile(filename, blob)
+	// if err != nil {
+	// 	return nil, err
+	// }
+
+	// return newFile, nil
 }
